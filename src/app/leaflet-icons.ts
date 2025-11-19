94a273b0ccb0fa99859a6f0edc0974292051a126
Aabid Rahman <aabidrahman001@gmail.com>
2025-11-19 22:47:49 +0600

    workflow fixup-5

--- src/app/leaflet-icons.ts
+++ src/app/leaflet-icons.ts
@@ -1,7 +1,5 @@
 import * as L from 'leaflet';
 
-delete L.Icon.Default.prototype._getIconUrl;
-
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
   iconUrl: 'assets/leaflet/marker-icon.png',
